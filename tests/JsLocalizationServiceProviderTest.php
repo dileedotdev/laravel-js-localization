6a3b7b5764dc186f08825dc87ae08a586ca26f18
dinhdjj <dinhdjj@gmail.com>
2022-05-09 08:44:14 +0700

    test: @jslocalization blade directive

--- tests/JsLocalizationServiceProviderTest.php
+++ tests/JsLocalizationServiceProviderTest.php
@@ -1,7 +1,21 @@
 <?php
 
+use Dinhdjj\JsLocalization\Facades\JsLocalization;
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\File;
 
 it('has jslocalization blade directive', function (): void {
     expect(Blade::getCustomDirectives())->toHaveKey('jslocalization');
+});
+
+test('@jslocalization blade directive', function (): void {
+    $result = Blade::getCustomDirectives()['jslocalization']();
+    $langs = json_encode(JsLocalization::getLangs());
+    $mainJs = File::get(__DIR__.'/../dist/main.js');
+
+    expect(str_contains($result, app()->getLocale()))->toBe(true);
+    expect(str_contains($result, '<script type="text/javascript">'))->toBe(true);
+    expect(str_contains($result, '</script>'))->toBe(true);
+    expect(str_contains($result, $langs))->toBe(true);
+    expect(str_contains($result, $mainJs))->toBe(true);
 });
